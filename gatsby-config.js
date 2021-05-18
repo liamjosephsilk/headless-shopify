223874c934786de29a0cd2afebfeb518fa0ea78c
Liam Joseph Silk <liamjosephsilk@gmail.com>
2021-05-18 22:30:31 +0100

    Remove unused packages. (#5)

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
         storeUrl: process.env.SHOPIFY_STORE_URL,
       },
     },
-    `@chakra-ui/gatsby-plugin`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -39,7 +38,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
